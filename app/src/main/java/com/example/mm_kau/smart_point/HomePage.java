dc5394e57c14933e8331c767058bc6f8c2876ab9
mazin2009 <mazin200940@gmail.com>
2018-08-03 01:35:14 +0300

    00

--- app/src/main/java/com/example/mm_kau/smart_point/HomePage.java
+++ app/src/main/java/com/example/mm_kau/smart_point/HomePage.java
@@ -1,30 +1,124 @@
 
 package com.example.mm_kau.smart_point;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomePage extends AppCompatActivity implements Designable {
 
 
     private TextView Points;
+    private TextView Name;
+    private ListView LV;
+    private ArrayList<REWARDS> ListOfReward;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Points = findViewById(R.id.TextPoints);
+        Name = findViewById(R.id.Name);
+
         Points.setText(getIntent().getStringExtra("Pointe"));
+      Name.setText("Mr. "+getIntent().getStringExtra("Name"));
         InitializeView();
     }
 
     @Override
     public void InitializeView() {
 
+
+
+        ListOfReward = new ArrayList<>();
+        this.LV = findViewById(R.id.ListView);
         this.Points = findViewById(R.id.TextPoints);
+
+        //Call Server to check the login Information.
+        StringRequest request = new StringRequest(Request.Method.POST, "https://hajj-reward-system.herokuapp.com/DB_getRewards.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("state");
+
+                    if (status.equals("yes")) {
+
+                        JSONArray companies = jsonObject.getJSONArray("companies");
+                        for (int i = 0; i < companies.length(); i++) {
+
+                      ListOfReward.add( new REWARDS(companies.get(i).toString()));
+                        }
+
+
+                        Adapter_Reword adapter = new Adapter_Reword(getBaseContext(), ListOfReward);
+                        LV.setAdapter(adapter);
+
+                        LV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                                Intent intent = new Intent(getBaseContext(), RE_Page.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.putExtra("comp", ListOfReward.get(i).getName());
+                                startActivity(intent);
+
+
+
+                            }
+                        });
+
+
+                    } else {
+                        Toast.makeText(getBaseContext(), "The ID not found", Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    Toast.makeText(getBaseContext(), "There is an error", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), "There is an error", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                // HTTP request parameters
+                HashMap<String, String> map = new HashMap<>();
+                return map;
+            }
+        };
+        // Add The volly request to the Singleton Queue.
+        Singleton_Queue.getInstance(getBaseContext()).Add(request);
+
+
         Design();
     }
 
--- app/src/main/java/com/example/mm_kau/smart_point/REWARDS.java
+++ app/src/main/java/com/example/mm_kau/smart_point/REWARDS.java
@@ -1,8 +1,40 @@
 package com.example.mm_kau.smart_point;
 
+import java.util.ArrayList;
+
 public class REWARDS {
 
-     private int ID ;
+     private String ID ;
      private String name;
+     private ArrayList<packages> redeem;
 
+    public void setRedeem(ArrayList<packages> redeem) {
+        this.redeem = redeem;
+    }
+
+    public REWARDS(String name) {
+        this.name = name;
+    }
+
+    public void setID(String ID) {
+
+          this.ID = ID;
+
+     }
+
+    public ArrayList<packages> getRedeem() {
+        return redeem;
+    }
+
+    public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getID() {
+          return ID;
+     }
+
+     public String getName() {
+          return name;
+     }
 }
